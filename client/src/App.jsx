ff75aecfb6c9d445d94ec917905426c33388adba
nobita <nobitagroup001@gmail.com>
2025-08-01 15:05:40 +0530

    Setup: Router setup

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,8 +1,11 @@
+import React from "react";
+import { Route, Routes } from "react-router-dom";
+
 function App() {
   return (
-    <>
-      <h1 class="text-3xl font-bold underline">Hello world!</h1>
-    </>
+    <Routes>
+      <Route path="/" element={<LoginPage />} />
+    </Routes>
   );
 }
 
