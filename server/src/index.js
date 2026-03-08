98c9a3e2bdf0445323f2a99568fcc2c8bd0bc5be
nobita <nobitagroup001@gmail.com>
2026-03-08 17:26:41 +0530

    updated index file

--- server/src/index.js
+++ server/src/index.js
@@ -1,11 +1,16 @@
 import app from "./app.js";
 import logger from "./config/logger.js";
-import "./whatsapp/whatsappClient.js";
-import "./crons/invoiceWhatsAppCrons.js";
-import "./crons/auditLogCleanupCrons.js"
+
+console.log("STEP 1: Starting server");
 
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
   logger.info(`Server running on port ${PORT} in ${process.env.NODE_ENV} mode`);
+  console.log("STEP 2: Express started");
 });
+
+// Start background services AFTER server is alive
+  await import("./whatsapp/whatsappClient.js");
+  await import("./crons/invoiceWhatsAppCrons.js");
+  await import("./crons/auditLogCleanupCrons.js");
